497ca61028c19196912b9703be2df681041ab076
Darmos <darmo.oneone12@gmail.com>
2023-07-05 10:26:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
+
+const url = 'https://api.telegram.org/bot';
+const apiToken = '5223743612:AAGgTZqngNqlYYvA7eDaxvRJMWyVKC23980';
+
+/*
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
+})
+*/
+
+app.post('/', (req,res) => {
+    axios.post(`${url}${apiToken}/sendMessage`,
+               {
+                    chat_id: '120611120',
+                    text: 'hello back 👋'
+               })
+               .then((response) => { 
+                    res.status(200).send(response);
+               }).catch((error) => {
+                    res.send(error);
+               });
 })
 app.listen(process.env.PORT || 3000)
